b78208ced32cf39026177cb2145926824213dc71
tarmopuurand <72023032+tarmopuurand@users.noreply.github.com>
2025-03-31 16:23:15 +0300

    Add files via upload

---
+++ model_training/distribution.pl
@@ -0,0 +1,27 @@
+# Turning points.
+# find treshold for sequemcing errors removal ( 1- x) and sequencing coverage (x) for normalization from glistquery --distribution output (Y-mer.pl)
+
+# | *
+# | .*
+# | ..*
+# | ...*
+# | ....*       ***
+# | .....*     * | *
+# | ......*   *  |  *
+# | .......***   |    *
+# | ........|    |       *****
+# |_________|____|______________
+#           |    |
+#       .errors  seq depth 
+$rida = 0;
+while(<>){
+   chomp;
+   @tmp = split(/\t/);
+   $jrk[$tmp[0]] = $tmp[1];
+   $jrk++;
+}
+for($i = 4; $i < $jrk; $i++){
+   print "$i" if ($jrk[$i] <  $jrk[$i-1] && $jrk[$i] <  $jrk[$i+1]);
+   print "\t$i\n" if ($jrk[$i] >  $jrk[$i-1] && $jrk[$i] >  $jrk[$i+1]);
+}
+   
